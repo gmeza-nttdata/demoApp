3d3d913a99716030ed7ad8ba74e7f160b29fe6f9
gmezan <meza.gustavo@pucp.edu.pe>
2022-04-30 19:06:20 -0500

    Implement simple logging

--- src/main/java/com/nttdata/training/demo/controller/DemoController.java
+++ src/main/java/com/nttdata/training/demo/controller/DemoController.java
@@ -1,13 +1,16 @@
 package com.nttdata.training.demo.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class DemoController {
 
     @GetMapping
     private String demo() {
+        log.info("Welcome to Updated Demo App - LOGGING");
         return "Welcome to Demo App";
     }
 
--- src/main/java/com/nttdata/training/demo/controller/UserDemoController.java
+++ src/main/java/com/nttdata/training/demo/controller/UserDemoController.java
@@ -2,10 +2,12 @@ package com.nttdata.training.demo.controller;
 
 import com.nttdata.training.demo.domain.User;
 import com.nttdata.training.demo.facade.DemoFacade;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+@Slf4j
 @RestController
 @RequestMapping("/demo/user")
 public class UserDemoController {
@@ -18,26 +20,31 @@ public class UserDemoController {
 
     @GetMapping
     public Flux<User> get() {
+        log.info("Get all users");
         return demoFacade.get();
     }
 
     @GetMapping("/{id}")
     public Mono<User> get(@PathVariable("id") String id) {
+        log.info("Get user by id");
         return demoFacade.getById(id);
     }
 
     @PostMapping
     public Mono<User> post(@RequestBody User user) {
+        log.info("Create user");
         return demoFacade.create(user);
     }
 
     @PutMapping("/{id}")
     public Mono<User> put(@PathVariable("id") String id, @RequestBody User user) {
+        log.info("Update user");
         return demoFacade.update(id, user);
     }
 
     @DeleteMapping("/{id}")
     public Mono<Void> delete(@PathVariable("id") String id) {
+        log.info("Delete user by id");
         return demoFacade.delete(id);
     }
 
