b493eb25400fdeca7fb4eb0d559322784838021c
gmezan <meza.gustavo@pucp.edu.pe>
2022-05-01 18:36:28 -0500

    Implementing Simple Cosmos Client

--- src/main/java/com/nttdata/training/demo/config/DemoConfiguration.java
+++ src/main/java/com/nttdata/training/demo/config/DemoConfiguration.java
@@ -1,5 +1,7 @@
 package com.nttdata.training.demo.config;
 
+import com.azure.cosmos.CosmosAsyncClient;
+import com.azure.cosmos.CosmosClientBuilder;
 import com.nttdata.training.demo.repository.MockUserRepository;
 import com.nttdata.training.demo.repository.UserRepository;
 import com.nttdata.training.demo.service.UserService;
@@ -10,6 +12,9 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class DemoConfiguration {
 
+    private static final String COSMOS_SERVICE_ENDPOINT = "COSMOS_SERVICE_ENDPOINT";
+    private static final String COSMOS_KEY = "COSMOS_KEY";
+
     @Bean
     public UserRepository userRepository() {
         return new MockUserRepository();
@@ -19,5 +24,14 @@ public class DemoConfiguration {
     public UserService userService(UserRepository userRepository) {
         return new UserServiceImpl(userRepository);
     }
+
+    @Bean
+    public CosmosAsyncClient cosmosAsyncClient() {
+        return new CosmosClientBuilder()
+                .endpoint(System.getenv(COSMOS_SERVICE_ENDPOINT))
+                .key(System.getenv(COSMOS_KEY))
+                .buildAsyncClient();
+    }
+
 
 }
