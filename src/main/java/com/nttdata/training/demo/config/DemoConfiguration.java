8500d633259b76ac19e73d097dac1b6bb42dbf77
gmezan <meza.gustavo@pucp.edu.pe>
2022-05-01 17:22:11 -0500

    Fixes in Config

--- src/main/java/com/nttdata/training/demo/config/DemoConfiguration.java
+++ src/main/java/com/nttdata/training/demo/config/DemoConfiguration.java
@@ -1,19 +1,23 @@
 package com.nttdata.training.demo.config;
 
+import com.nttdata.training.demo.repository.MockUserRepository;
+import com.nttdata.training.demo.repository.UserRepository;
+import com.nttdata.training.demo.service.UserService;
+import com.nttdata.training.demo.service.UserServiceImpl;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class DemoConfiguration {
 
-    /*
     @Bean
-    public UserRepository getUserRepository() {
+    public UserRepository userRepository() {
         return new MockUserRepository();
     }
 
     @Bean
-    public UserService getUserService(UserRepository userRepository) {
+    public UserService userService(UserRepository userRepository) {
         return new UserServiceImpl(userRepository);
-    }*/
+    }
 
 }
