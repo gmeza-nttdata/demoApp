12e50f07a94b729cff227aa3559f6a868a827128
gmezan <meza.gustavo@pucp.edu.pe>
2022-05-11 16:56:00 -0500

    Documentation with Swagger

--- src/main/java/com/nttdata/training/demo/controller/DemoController.java
+++ src/main/java/com/nttdata/training/demo/controller/DemoController.java
@@ -1,5 +1,6 @@
 package com.nttdata.training.demo.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoController {
 
+    @Operation(
+            summary = "1.0 Say Hi Operation"
+    )
     @GetMapping
     private String demo() {
         log.info("Welcome to Updated Demo App - LOGGING");
--- src/main/java/com/nttdata/training/demo/controller/UserDemoController.java
+++ src/main/java/com/nttdata/training/demo/controller/UserDemoController.java
@@ -2,6 +2,7 @@ package com.nttdata.training.demo.controller;
 
 import com.nttdata.training.demo.domain.User;
 import com.nttdata.training.demo.facade.DemoFacade;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
@@ -18,30 +19,35 @@ public class UserDemoController {
         this.demoFacade = demoFacade;
     }
 
+    @Operation(summary = "2.1 Get Users")
     @GetMapping
     public Flux<User> get() {
         log.info("Get all users");
         return demoFacade.get();
     }
 
+    @Operation(summary = "2.2 Get User by Id")
     @GetMapping("/{id}")
     public Mono<User> get(@PathVariable("id") String id) {
         log.info("Get user by id");
         return demoFacade.getById(id);
     }
 
+    @Operation(summary = "2.3 Create User")
     @PostMapping
     public Mono<User> post(@RequestBody User user) {
         log.info("Create user");
         return demoFacade.create(user);
     }
 
+    @Operation(summary = "2.4 Update User by Id")
     @PutMapping("/{id}")
     public Mono<User> put(@PathVariable("id") String id, @RequestBody User user) {
         log.info("Update user");
         return demoFacade.update(id, user);
     }
 
+    @Operation(summary = "2.5 Delete User by Id")
     @DeleteMapping("/{id}")
     public Mono<Void> delete(@PathVariable("id") String id) {
         log.info("Delete user by id");
